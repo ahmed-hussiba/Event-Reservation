3257ef2bb893c06dd5d8afc99002cff85f4f7d77
Mokhtar Mostafa Mokhtar Muhammad <mokhtarmostafa1526@gmail.com>
2024-03-26 00:09:48 +0200

    test

--- Backend/Models/AdminModel.js
+++ Backend/Models/AdminModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const validators = require('validator')
-
+/////////
 const adminSchema = new mongoose.Schema({
     _id:{type:Number,required:true},
     email:{type:String,required:true, pattern: "^[a-zA-Z0-9._%+-]+@admin.com$"},

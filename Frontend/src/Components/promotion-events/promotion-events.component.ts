fa520047e2b0db80c53c08d34c074e94d087f595
Mokhtar Mostafa Mokhtar Muhammad <mokhtarmostafa1526@gmail.com>
2024-04-01 02:59:12 +0200

    sssss

---
+++ Frontend/src/Components/promotion-events/promotion-events.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promotion-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './promotion-events.component.html',
+  styleUrl: './promotion-events.component.css'
+})
+export class PromotionEventsComponent {
+
+}

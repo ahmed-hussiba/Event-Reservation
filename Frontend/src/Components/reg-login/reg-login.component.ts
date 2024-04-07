cd2e7a3c7eb956facb3a09eba445eaf815f75c6f
yasmeen-mohy <130379431+yasmeen-mohy@users.noreply.github.com>
2024-04-07 03:05:09 +0200

    register Image

--- Frontend/src/Components/reg-login/reg-login.component.ts
+++ Frontend/src/Components/reg-login/reg-login.component.ts
@@ -21,7 +21,8 @@ export class RegLoginComponent {
   user:any;
   responseData:any;
   responseHeaders:any;
-  constructor(private logService:LoginService){
+  image:any;
+  constructor(private logService:LoginService,private regService:RegisterService){
 
   }
   mySignInFormGroup = new FormGroup(
@@ -30,7 +31,17 @@ export class RegLoginComponent {
       password: new FormControl("", [Validators.pattern(
         "^(?=.*[a-zA-Z])(?=.*\\d).{8,}$"
         ),Validators.required]) 
-    })
+    });//
+    mySignUpFormGroup = new FormGroup({
+      email: new FormControl("", [Validators.email, Validators.required]),
+      username: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(15), Validators.pattern("^[a-zA-Z0-9_.]+$")]),
+      password: new FormControl("", [Validators.pattern(
+        "^(?=.*[a-zA-Z])(?=.*\\d).{8,}$"
+        ),Validators.required]) ,
+      country: new FormControl("", [Validators.required]),
+      city: new FormControl("", [Validators.required]),
+      fileUpload: new FormControl("", [Validators.required]) // Add the file validator here
+    });
 
   get emailIsValid(){
     return this.mySignInFormGroup.controls["email"].valid;
@@ -65,8 +76,11 @@ export class RegLoginComponent {
 
     if (this.mySignUpFormGroup.valid) {
       this.user = this.mySignUpFormGroup.value;
-      console.log(this.user);
-      this.regService.signUp(this.user).subscribe(
+      const formData = new FormData();
+      formData.append('image',this.image);
+      formData.append('data',this.user);
+      console.log(formData);
+      this.regService.signUp(formData).subscribe(
         {
           next: (data) => {
             console.log(data);
@@ -100,81 +114,20 @@ export class RegLoginComponent {
     }
   }
 
-  triggerFileInput() {
+  triggerFileInput(event:any) {
     const fileInput = document.getElementById('fileInput') as HTMLInputElement;
     fileInput.click();
-  }
-}
-import { CommonModule } from '@angular/common';
-import { jwtDecode } from 'jwt-decode';
-import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { LoginService } from '../../Services/login.services';
-import {
-  HttpClient,
-  HttpClientModule,
-  HttpResponse,
-} from '@angular/common/http';
-import { Router } from '@angular/router';
-import { routes } from '../../app/app.routes';
+    console.log(fileInput.files);
 
-@Component({
-  selector: 'app-reg-login',
-  standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
-  providers: [LoginService],
-  templateUrl: './reg-login.component.html',
-  styleUrl: './reg-login.component.css',
-})
-export class RegLoginComponent {
-  user: any;
-  responseData: any;
-  responseHeaders: any;
-  constructor(private logService: LoginService, private route: Router) {}
-  mySignInFormGroup = new FormGroup({
-    email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [
-      Validators.pattern('^(?=.*[a-zA-Z])(?=.*\\d).{8,}$'),
-      Validators.required,
-    ]),
-  });
-
-  get emailIsValid() {
-    return this.mySignInFormGroup.controls['email'].valid;
-  }
-  get passwordIsValid() {
-    return this.mySignInFormGroup.controls['password'].valid;
-  }
-
-  submitSignIn() {
-    if (this.mySignInFormGroup.valid) {
-      this.user = this.mySignInFormGroup.value;
-      console.log(this.user);
-
-      this.logService.signIn(this.user).subscribe({
-        next: (data) => {
-          const authToken = data.headers.get('x-auth-token');
-          // console.log('x-auth-token:', authToken);
-          console.log(JSON.stringify(authToken));
-
-          localStorage.setItem('access_token', JSON.stringify(authToken));
-
-          // console.log(decoded);
-          window.location.reload();
-
-          // this.route.navigate(['/']);
-          // const decodedToken = jwt_decode();
-          // console.log(decodedToken);
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
+    if (fileInput.files?.length) {
+      const file = fileInput.files[0];
+      this.image = file;
+      console.log(this.image);
+    
+      
+      
     }
   }
 }
+
+
